8faf5169aa32387dea11e7adab13b7ceb7473e9b
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-03-18 16:15:15 +0800

    fix: 修改docker-compose

--- clinic-app/src/middleware/auth.middleware.ts
+++ clinic-app/src/middleware/auth.middleware.ts
@@ -1,8 +1,7 @@
 // src/middleware/auth.middleware.ts
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, HttpException, HttpStatus } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { JwtService } from '@nestjs/jwt';
-import { HttpException, HttpStatus } from '@nestjs/common';
 import { jwtConstants } from 'src/jwt.config';
 import { refreshAccessToken } from 'src/utils/auth.utils';
 // 确保你有常量存放 JWT 密钥
@@ -15,7 +14,6 @@ export class AuthMiddleware implements NestMiddleware {
     // 获取头部信息
     const accessToken = req.headers['authorization']?.split(' ')[1];
     const refreshToken = req.headers['x-refresh-token'];
-    console.log('wzy999 accessToken ', accessToken);
 
     if (!accessToken || !refreshToken) {
       throw new HttpException('未认证', HttpStatus.UNAUTHORIZED);
@@ -23,15 +21,14 @@ export class AuthMiddleware implements NestMiddleware {
 
     try {
       // 校验 accessToken
-      const payload = this.jwtService.verify(accessToken, {
+      this.jwtService.verify(accessToken, {
         secret: jwtConstants.secret,
       });
-      req.user = payload;  // 把用户信息加到请求对象上
       return next();  // 如果验证通过，继续执行后续的请求处理
     } catch (err) {
       if (err.name === 'TokenExpiredError') {
         // 如果 accessToken 过期，尝试使用 refreshToken 获取新 token
-        res.json(refreshAccessToken(refreshToken, res, this.jwtService));
+        res.json(refreshAccessToken(refreshToken as string, res, this.jwtService));
         return next();
       }
 
