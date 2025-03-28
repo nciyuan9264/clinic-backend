c85c7430bccc52a38bafccb891c28b96a3ac906a
WangZhenyu <wangzhenyu.9264@bytedance.com>
2025-03-28 11:13:52 +0800

    fix: 跨域

--- clinic-app/src/main.ts
+++ clinic-app/src/main.ts
@@ -6,9 +6,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   // 启用 CORS
+
+  const allowedOrigins = [
+    'http://localhost',
+    'https://yandaifu.xyz',
+    'https://www.yandaifu.xyz',
+  ];
+
   app.enableCors({
-    origin: ['http://localhost', 'https://yandaifu.xyz', 'https://www.yandaifu.xyz'],
-    credentials: true, // 支持 Cookie
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin); // 允许请求的 Origin
+      } else {
+        callback(new Error('Not allowed by CORS')); // 拒绝不在列表中的 Origin
+      }
+    },
+    credentials: true, // 允许 Cookie
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   });
